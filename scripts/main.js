bdf92eb3947f72b34b20279cbd3f24a4a1f19f43
Erick MG <lemg040798@gmail.com>
2020-09-09 14:43:46 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myButton.onclick = function() {
+    setUserName();
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  
